a745aa5e7c3d60504cd381b8155bcc6c9402085d
BAJJURI MAHENDAR REDDY <61425460+mahendar444@users.noreply.github.com>
2022-06-14 20:36:01 +0530

    Added Welcome Message

--- src/main/java/com/pack/jenkins/test/HelloJenkins.java
+++ src/main/java/com/pack/jenkins/test/HelloJenkins.java
@@ -15,6 +15,8 @@ public class HelloJenkins {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("*********** Hello JENKINS ***********");
+
 	}
 
 }
