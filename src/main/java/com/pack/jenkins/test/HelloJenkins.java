b924509fef1890893669261f942bf6568c050183
BAJJURI MAHENDAR REDDY <61425460+mahendar444@users.noreply.github.com>
2022-06-14 20:33:13 +0530

    Second Commit

---
+++ src/main/java/com/pack/jenkins/test/HelloJenkins.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.pack.jenkins.test;
+
+/**
+ * @author MAHENDAR REDDY
+ *
+ */
+public class HelloJenkins {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
